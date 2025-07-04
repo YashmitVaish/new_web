459b4e1d48c3b6408b9c88545dd71067ade32b05
Abhinav Saini <abhinavsaini.5106@gmail.com>
2025-07-04 17:56:52 +0530

    changes

--- src/components/springer.jsx
+++ src/components/springer.jsx
@@ -3,11 +3,11 @@ import { Box, Typography, Link } from '@mui/material';
 
 const SpringerInfo = ({ data }) => {
   return (
-    <Box sx={{ mt: 3 }}>
+    <Box sx={{ mt: 3 , fontWeight: '900'}}>
       {data.map((item, index) => {
         if (item.type === 'text') {
           return (
-            <Typography key={index} variant="body1" component="span">
+            <Typography key={index} variant="body1" component="span"sx={{ fontWeight: 'bold' }}>
               {item.value}
             </Typography>
           );
@@ -23,17 +23,6 @@ const SpringerInfo = ({ data }) => {
             >
               {item.value}
             </Link>
-          );
-        }
-        if (item.type === 'image') {
-          return (
-            <Box
-              key={index}
-              component="img"
-              src={item.src}
-              alt={item.alt}
-              sx={{ width: '100%', height: 'auto', mt: 2, borderRadius: 2 }}
-            />
           );
         }
         return null;
